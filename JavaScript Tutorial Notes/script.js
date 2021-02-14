2fbdb91c2ca6a742ae331ff17049bbf60d4c06a5
Sujayraj-Boregouda <sujayraj.gouda@gmail.com>
2021-02-14 23:21:57 +0530

    JSWIK 61 - 70

--- JavaScript Tutorial Notes/script.js
+++ JavaScript Tutorial Notes/script.js
@@ -1,10 +1,37 @@
-function istrue(){
-    if (true){
-        return "true";
-    }
-    else {
-        return "false";
-    }
-}
+/*
 
-console.log(false)
+function testSize(num) {
+    if (num < 5) {
+    return "Tiny"
+    } else if (num < 10) {
+    return "small"
+    } else if (num < 15) {
+    return "medium"
+    } else if (num < 20) {
+    return "large"
+    } else {
+    return "huge"
+    }
+    }
+
+    console.log(testSize(30)) 
+
+    */
+
+
+    function testSize(num){
+        if (num < 5){
+            return "less than 5";
+        } else if (num < 10){
+            return "less than 10";
+        } else if (num < 15){
+            return "less than 15";
+        } else if (num < 20){
+           return "less than 20" 
+        } else {
+            return "more than 25"
+        }
+    }
+
+
+    console.log(testSize(15))
