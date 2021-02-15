1b28c5ef57eb13d1700df502e60d54a9ff5a7f08
Sujayraj-Boregouda <sujayraj.gouda@gmail.com>
2021-02-15 23:22:53 +0530

    JSWIK 75 - 80

--- JavaScript Tutorial Notes/script.js
+++ JavaScript Tutorial Notes/script.js
@@ -1,37 +1,27 @@
-/*
 
-function testSize(num) {
-    if (num < 5) {
-    return "Tiny"
-    } else if (num < 10) {
-    return "small"
-    } else if (num < 15) {
-    return "medium"
-    } else if (num < 20) {
-    return "large"
-    } else {
-    return "huge"
+function functionToSwitch(val) {
+    var answer = "";
+
+    switch(val) {
+    case "bob":
+    answer = "marley";
+    break;
+    case 42:
+    answer = "The answer";
+    break;
+    case 1:
+    answer = "There is no #1";
+    break;
+    case 99:
+    answer = "Missed me by this much!";
+    break;
+    case 7:
+    answer = "Ate Nine";
+    break;
     }
-    }
-
-    console.log(testSize(30)) 
-
-    */
+    return answer;
+}
+    console.log(functionToSwitch(val))
 
 
-    function testSize(num){
-        if (num < 5){
-            return "less than 5";
-        } else if (num < 10){
-            return "less than 10";
-        } else if (num < 15){
-            return "less than 15";
-        } else if (num < 20){
-           return "less than 20" 
-        } else {
-            return "more than 25"
-        }
-    }
-
-
-    console.log(testSize(15))
+    
