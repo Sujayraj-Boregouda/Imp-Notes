44d412ccd8e37d49d64a6ef60d408f300de0793a
Sujayraj-Boregouda <sujayraj.gouda@gmail.com>
2021-03-14 23:51:28 +0530

    JSWIK last update of Fcc

--- JavaScript Tutorial Notes/script.js
+++ JavaScript Tutorial Notes/script.js
@@ -1,3 +1,37 @@
-let x = 3 === "3";
+ /*
+ function makeClass(){
+     class vegetable{
+         constructor (name){
+             this.name = name;
+         }
+     }
+     return vegetable;
+ }
 
-console.log(x)
+
+ const vegetable = makeClass();
+ 
+ 
+ const beetroot = new vegetable ("beetroot");
+
+ console.log(beetroot.name)
+
+ */
+
+
+ function makeClass(){
+     class vegetable{
+         constructor (name){
+             this.name = name;
+         }
+     }
+     return vegetable;
+ }
+
+
+ const vegetable = makeClass();
+
+ const carrot = new vegetable("carrot")
+
+ console.log(carrot.name)
+
